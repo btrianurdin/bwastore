2501346a26698af0a5fa50d2a9e0748d6df71604
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-09-06 17:29:42 +0700

    refactor: change endpoint player -> players

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -9,15 +9,15 @@ import DashboardController from "../controllers/api/DashboardController";
 const router = Router();
 
 router
-  .get("/player/landingpage", PlayerController.landingPage)
-  .get("/player/:id/detail", PlayerController.detailPage)
-  .get("/player/category", PlayerController.allCategory)
-  .post("/player/checkout", ApiAuthMiddleware, PlayerController.checkout)
-  .get("/player/history", ApiAuthMiddleware, DashboardController.history)
-  .get("/player/history/:id", ApiAuthMiddleware, DashboardController.historyDetail)
-  .get("/player/dashboard", ApiAuthMiddleware, DashboardController.dashboard)
-  .get("/player/profile", ApiAuthMiddleware, PlayerController.profile)
-  .put("/player/profile", 
+  .get("/players/landingpage", PlayerController.landingPage)
+  .get("/players/:id/detail", PlayerController.detailPage)
+  .get("/players/category", PlayerController.allCategory)
+  .post("/players/checkout", ApiAuthMiddleware, PlayerController.checkout)
+  .get("/players/history", ApiAuthMiddleware, DashboardController.history)
+  .get("/players/history/:id", ApiAuthMiddleware, DashboardController.historyDetail)
+  .get("/players/dashboard", ApiAuthMiddleware, DashboardController.dashboard)
+  .get("/players/profile", ApiAuthMiddleware, PlayerController.profile)
+  .put("/players/profile", 
     ApiAuthMiddleware, 
     multer({ storage: multerStorage }).single("avatar"),
     PlayerController.profileEdit
